115cd1fe2972c92d245122315325bd12f3354fcb
laychan25 <lauriesilva0@gmail.com>
2024-04-08 14:19:25 -0300

    commit

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import Botao from '../Botao'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './formulario.css'
+
+const Formulario = ({aoCadastrar, times , cadastrarTime}) => {
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+    const [nomeTime, setnomeTime] = useState('')
+    const [corTime, setcorTime] = useState('')
+
+    const aoSubmeter = (evento) => {
+        evento.preventDefault()
+        console.log('form enviado', nome, cargo, imagem, time )
+        aoCadastrar({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+    }
+
+    return (
+        <section className="formulario-container">
+            <form className="formulario" onSubmit={aoSubmeter}>
+                <h2>Preencha os dados para criar o card do colaborador.</h2>
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Nome'
+                    placeholder='Digite seu nome '
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}/>
+                <CampoTexto
+                    obrigatorio={true}
+                    label='Cargo' 
+                    placeholder='Digite seu cargo '
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}/>
+                <CampoTexto 
+                    label='Imagem' 
+                    placeholder='Informe o endereço da imagem '
+                    aoAlterado={valor => setImagem(valor)}/>
+                <ListaSuspensa 
+                    obrigatorio={true}
+                    label='Times'
+                    items={times} 
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}/>
+                <Botao texto='Criar card' />
+            </form>
+            <form className="formulario" onSubmit={(evento)=> {
+                evento.preventDefault()
+                cadastrarTime({nome: nomeTime, cor:corTime})
+            }}>
+                <h2>Preencha os dados para criar um novo time.</h2>
+                <CampoTexto
+                    obrigatorio
+                    label='Nome'
+                    placeholder='Digite o nome do time '
+                    valor={nomeTime}
+                    aoAlterado={valor => setnomeTime(valor)}
+                />
+                <CampoTexto
+                    obrigatorio
+                    label='cor' 
+                    placeholder='Digite a cor do seu time '
+                    valor={corTime}
+                    aoAlterado={valor => setcorTime(valor)}
+                />
+                <Botao texto='Criar novo time' />
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
